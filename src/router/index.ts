0474d534317a8b552403e0dc1d41d8e6a32cb123
icarodredd <icarog854@gmail.com>
2025-10-30 15:41:21 -0300

    feat: add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,6 @@
+import AccountView from '@/views/account-view.vue'
+import ChatsView from '@/views/chats-view.vue'
+import PrivacyView from '@/views/privacy-view.vue'
 import SettingsView from '@/views/settings-view.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
@@ -11,6 +14,18 @@ const router = createRouter({
     {
       path: '/settings',
       component: SettingsView,
+    },
+    {
+      path: '/settings/account',
+      component: AccountView,
+    },
+    {
+      path: '/settings/privacy',
+      component: PrivacyView,
+    },
+    {
+      path: '/settings/chats',
+      component: ChatsView,
     },
   ],
 })
