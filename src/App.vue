ccd1bd293e0d1888dfdf7e60846e81d77b5d2d39
icarodredd <icarog854@gmail.com>
2025-10-29 21:43:24 -0300

    feat: layout settings

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AutoComplete, { type AutoCompleteCompleteEvent } from 'primevue/autocomplete'
+import { ref } from 'vue'
+
+const value = ref('')
+const items = ref(['Configuração 1', 'Configuração 2', 'Configuração 3'])
+
+const search = (event: AutoCompleteCompleteEvent) => {
+  items.value = [...Array(10).keys()].map((item) => event.query + '-' + item)
+}
+</script>
 
 <template>
-  <h1 class="bg-blue-500">You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="m-4 flex flex-col gap-4">
+    <h1 class="text-2xl font-bold">Configurações</h1>
+    <AutoComplete variant="filled" v-model="value" :suggestions="items" @complete="search" />
+  </div>
+
+  <router-view />
 </template>
 
 <style scoped></style>
---
+++ src/views/settings-view.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const configurations = ref([{ name: 'Conta' }, { name: 'Privacidade' }, { name: 'Chats' }])
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style></style>
