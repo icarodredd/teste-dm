c9f71ae53a99eb8d4c444fab118cce7ddf6016d5
icarodredd <icarog854@gmail.com>
2025-10-30 14:59:29 -0300

    feat: finish settings view

--- src/views/settings-view.vue
+++ src/views/settings-view.vue
@@ -1,5 +1,6 @@
-<script setup lang="ts"></script>
-
-<template>configurations</template>
-
-<style></style>
+<template>
+  <div class="h-screen flex flex-col justify-center items-center">
+    <i class="pi pi-cog text-green-400" style="font-size: 5rem" />
+    <h1 class="text-2xl font-bold text-green-400 mt-4">Configurações</h1>
+  </div>
+</template>
