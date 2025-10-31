b49a582c90987ab69d02f00b27afc35223fe50c6
icarodredd <icarog854@gmail.com>
2025-10-31 10:37:32 -0300

    style: text-center

--- src/components/sign-out.vue
+++ src/components/sign-out.vue
@@ -12,7 +12,7 @@ const closeDialog = () => {
 
 <template>
   <div class="h-full w-full flex flex-col justify-items-center gap-10">
-    <h2 class="font-black text-4xl">Tem certeza que deseja sair?</h2>
+    <h2 class="font-black text-4xl text-center">Tem certeza que deseja sair?</h2>
     <div class="flex gap-8 justify-center">
       <Button label="Cancelar" severity="success" @click="closeDialog" />
       <Button
